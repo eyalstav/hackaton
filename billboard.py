586f132bf050a09bab48db853889d9507035c8a0
danielT32 <danialzimk@gmail.com>
2023-08-30 16:35:20 +0300

    added search bar and create issue

--- billboard.py
+++ billboard.py
@@ -1,35 +1,41 @@
+import json
+
 from kivy.lang import Builder
 from kivy.properties import ListProperty
+from kivy.uix.screenmanager import Screen, ScreenManager
 from kivymd.app import MDApp
 from kivymd.uix.list import OneLineAvatarIconListItem
 from kivymd.uix.textfield import MDTextField
-from firebase import firebase
+import requests
 
 
-# Builder.load_file('project.kv')
+class FirstWindow(Screen):
+    pass
 
 
-# class MyGridLayout(GridLayout):
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         self.cols = 2
-#         self.search_bar = TextInput(multiline=False)
-#         self.add_widget(self.search_bar)
-#         self.search = Button().add_widget(Image())
+class SecondWindow(Screen):
+    pass
+
+
+class WindowManager(ScreenManager):
+    pass
 
 
 class Search_Select_Option(OneLineAvatarIconListItem):
     def show_issue(self):
         pass
+        # show issue screen
 
 
 def get_issues_text_from_db():
-    fb_app = firebase.FirebaseApplication(
-        'https://communityconnect1234-default-rtdb.europe-west1.firebasedatabase.app/', None)
-    billboard_db_data = fb_app.get('/Billboard', None)
+    res = requests.get(
+        'https://communityconnect1234-default-rtdb.europe-west1.firebasedatabase.app/Billboard.json')
+    billboard_db_data = res.json()
+    print(billboard_db_data)
     option_list = ""
     for dictionary in billboard_db_data:
-        option_list += ','.join([msg for msg in dictionary.values()]) + ","
+        option_list += dictionary
+        option_list += ","
     option_list = option_list.split(',')
     return option_list
 
@@ -66,18 +72,30 @@ class SearchTextInput(MDTextField):
 class RVTestApp(MDApp):
     rv_data = ListProperty()
 
+    def reset_data_to_default(self):
+        self.rv_data = [{'text': item} for item in get_issues_text_from_db()]
+
     def update_data(self, rv_data_list):
         self.rv_data = [{'text': item} for item in rv_data_list]
         print(self.rv_data, 'update')
 
     def build(self):
-        self.rv_data = [{'text': item} for item in get_issues_text_from_db()]
+        self.reset_data_to_default()
         return Builder.load_file("project.kv")
 
     def erase(self):
-        self.rv_data = []
+        self.reset_data_to_default()
 
     def search(self):
+        pass
+
+    def insert_issue_to_db(self, topic, description):
+        if topic != "" and description != "":
+            print(topic + "," + description)
+            # patch db
+            res = requests.patch(
+                'https://communityconnect1234-default-rtdb.europe-west1.firebasedatabase.app/Billboard.json',
+                data=json.dumps({topic: {'description': description}}))
         pass
 
 
